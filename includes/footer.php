347c09aa8c7980be42654d14ae217d34cfc3fdc8
AmalieAA <124889573+AmalieAA@users.noreply.github.com>
2023-05-25 17:44:05 +0200

    Footer oprettet

---
+++ includes/footer.php
@@ -0,0 +1,60 @@
+<footer class="container-fluid">
+
+    <div class="row bg-baggrund border-top border-borderfarve font-footer text-success p-2">
+
+        <div class="col-12">
+            <p>
+                <strong>
+                    Veteranhaven
+                </strong>
+                <br>
+                <br>
+                Brovej 13B, Slots Bjergby
+                <br>
+                4200 Slagelse
+                <br>
+                <br>
+            </p>
+        </div>
+        <div class="col-12">
+            <p>
+                <i class="fa-solid fa-phone-flip"></i>
+                Tlf: 52 40 04 55
+                <br>
+                <i class="fa-solid fa-envelope"></i>
+                E-mail: administration@veteranhaven.dk
+                <br>
+                <br>
+                <i class="fa-solid fa-arrow-right"></i>
+                www.veteranhaven.dk
+                <br>
+                <br>
+            </p>
+
+        </div>
+
+
+        <div class="col-12">
+            <p>
+                Find os på
+                <br>
+                <i class="fa-brands fa-facebook fa-2x"></i>
+                <i class="fa-brands fa-linkedin fa-2x"></i>
+                <br>
+                <br>
+            </p>
+
+        </div>
+
+
+        <div class="col-12 text-center copyright-icon">
+            <p>
+                Copyright
+                <i class="fa-regular fa-copyright"></i>
+                Alle rettigheder forbeholdes
+            </p>
+
+        </div>
+    </div>
+
+</footer>
--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 	<meta name="copyright" content="Information om copyright">
 	
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/ddc56212a6.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Serif&display=swap" rel="stylesheet">
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -18,6 +22,7 @@
 
 
 <?php include "includes/header.php";?>
+<?php include "includes/footer.php";?>
 <div class="container-fluid">
 </div>
 
